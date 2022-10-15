61e1d20a42ac4635c572736c310d2be8253c15b2
Abhijith.M.K <105772028+Abhijith-M-K@users.noreply.github.com>
2022-10-15 09:08:10 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import ToDo from './components/ToDo';
+import Button from 'react-bootstrap/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+     <ToDo />
+    </div>
+  );
+}
+
+export default App;
